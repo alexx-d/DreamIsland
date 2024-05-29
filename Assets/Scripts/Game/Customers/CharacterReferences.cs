32e82d3d24b6b626ca5f035efd2acf895d9cbbb5
alexx-d <lexadyakov@gmail.com>
2024-05-29 11:25:44 +0300

    Initial commit

---
+++ Assets/Scripts/Game/Customers/CharacterReferences.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+//todo split references for different characters
+public class CharacterReferences : MonoBehaviour
+{
+    [SerializeField] private List<TimerStackableProducer> _producers;
+
+    [field: SerializeField] public ConveyorsContainer ConveyorsContainer { get; private set; }
+    [field: SerializeField] public Transform ExitPoint { get; private set; }
+    [field: SerializeField] public Conveyor CrematoryConveyor { get; private set; }
+    [field: SerializeField] public Transform UrnStorage { get; private set; }
+    [field: SerializeField] public StackPresenter UrnStack { get; private set; }
+
+    public IEnumerable<TimerStackableProducer> Producers => _producers;
+}
---
+++ Assets/Scripts/Game/Shop/Conveyor/CashDesk.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class CashDesk : LockedGameObject
+{
+
+    [SerializeField] private MoneyZone _moneyZone;
+    [SerializeField] private Transform _servePoint;
+    [SerializeField] private Transform _customerWaitPoint;
+    [SerializeField] private StackPresenter _tombstoneStack;
+    [SerializeField] private List<StackPresenter> _coffinStacks;
+
+    public event Action BecameFree;
+
+    public bool Active { get; private set; }
+    public bool Free { get; private set; } = true;
+    public Transform ServePoint => _servePoint;
+
+    private void Start() =>
+        SetActive(false);
+
+    public void Leave()
+    {
+        SetActive(false);
+        Free = true;
+        BecameFree?.Invoke();
+    }
+
+    public void Place(IEnumerable<Stackable> stackData)
+    {
+        for (int i = 0; i < stackData.Count(); i++)
+            _coffinStacks[i].AddToStack(stackData.ElementAt(i));
+    }
+
+    private void SetActive(bool active) =>
+        Active = active;
+
+    private IEnumerator MoneyZoneActivationDelay()
+    {
+        yield return new WaitForSeconds(1f);
+        _moneyZone.SetActive(true);
+    }
+}
